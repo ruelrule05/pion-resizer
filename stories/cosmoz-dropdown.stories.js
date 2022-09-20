104848dee25539ea3fd3ec03b1dbcadf1fc326f8
Meghea Iulian <iulian.meghea@gmail.com>
2022-09-20 10:20:02 +0300

    fix(ci): add DropdownMenu story

--- stories/cosmoz-dropdown.stories.js
+++ stories/cosmoz-dropdown.stories.js
@@ -12,6 +12,17 @@ const
 		<div>Item 4</div>
 		<div>Item 5</div>
 		<button>Item 6</div>
-	</cosmoz-dropdown>`;
+	</cosmoz-dropdown>`,
 
-export { Dropdown };
+
+	DropdownMenu = () => `<cosmoz-dropdown-menu>
+		<div>Item 1</div>
+		<div>Item 2</div>
+		<div>Item 3</div>
+		<div>Item 4</div>
+		<div>Item 5</div>
+		<a href="#">Achor 1</a>
+	</cosmoz-dropdown-menu>`;
+
+
+export { Dropdown, DropdownMenu };

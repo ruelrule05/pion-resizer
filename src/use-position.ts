f36acedeb5ffb6e944e3a39d079aec4a6c374ed2
Amanda Valencia <amanda.valencia@neovici.se>
2024-10-04 16:26:04 +0200

    fix(use-position): add if rule when element end up outside viewport

--- src/use-position.ts
+++ src/use-position.ts
@@ -39,7 +39,11 @@ export const position = ({
 			offsetParent: confinement,
 		}),
 		{ style } = host;
-	style.left = offset.left + 'px';
+	if (offset.left < 0) {
+		style.left = '0px';
+	} else {
+		style.left = offset.left + 'px';
+	}
 	style.top = offset.top + 'px';
 	if (limit) {
 		style.minWidth = Math.max(anchorBounds.width, hostBounds.width) + 'px';
